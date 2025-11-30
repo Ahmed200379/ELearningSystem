33d3b38b76121307cc4a3d6546333fc92e940b2e
Rw <Rw@AHMED>
2025-11-30 16:46:43 +0200

    add auth controller

--- ELearningSystem/Controllers/AuthController.cs
+++ ELearningSystem/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Shared.Dtos.Auth;
 
 namespace ELearningSystem.Controllers
@@ -7,6 +8,70 @@ namespace ELearningSystem.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var result = await _authService.Register(registerDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpPost("verify otp")]
+        public async Task<IActionResult> VerifyOtp([FromBody] VerifyOtpDto verifyOtpDto)
+        {
+            var result = await _authService.VerifyOtp(verifyOtpDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _authService.Login(loginDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpPost("forget password")]
+        public async Task<IActionResult> ForgetPassword([FromBody] string email)
+        {
+            var result = await _authService.ForgetPassword(email);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpPost("verify otp for password")]
+        public async Task<IActionResult> VerifyCode([FromBody] VerifyOtpDto verifyOtpDto)
+        {
+            var result = await _authService.VarifyOtpForPassword(verifyOtpDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
 
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
+        {
+            var result = await _authService.ResetPassword(resetPasswordDto);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
     }
 }
---
+++ ELearningSystem/Properties/ApplicationServices.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using Persistence.Repos;
+using Services;
+
+namespace ELearningSystem.Properties
+{
+    public class ApplicationServices
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthServices>();
+            services.AddScoped<IGroupServices, GroupServices>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IJwtRepo, JwtRepo>();
+        }
+    }
+}
