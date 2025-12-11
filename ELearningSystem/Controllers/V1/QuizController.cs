c8e3c1651608641f4ef8a19293b8e88bdac98cc5
ahmedessam228 <141870029+ahmedessam228@users.noreply.github.com>
2025-12-11 18:01:24 +0200

    add-controller(quiz , answer , quest)

---
+++ ELearningSystem/Controllers/V1/QuizController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared.Dtos.Quiz;
+
+namespace ELearningSystem.Controllers.V1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizService _quizService;
+        public QuizController(IQuizService quizService)
+        {
+            _quizService = quizService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllQuizzes()
+        {
+            var quizzes = await _quizService.GetAll();
+            return Ok(quizzes);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddQuiz([FromBody] RequestQuizDto quiz)
+        {
+            var addedQuiz = await _quizService.Add(quiz);
+            return Ok(addedQuiz);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateQuiz(string id, [FromBody] RequestUpdateQuizeDto quiz)
+        {
+            try
+            {
+                var updatedQuiz = await _quizService.Update(id, quiz);
+                return Ok(updatedQuiz);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteQuiz([FromQuery] string id)
+        {
+            try
+            {
+                var result = await _quizService.Delete(id);
+                return Ok(result);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
--- ELearningSystem/Program.cs
+++ ELearningSystem/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistence.Data;
 using Shared.Helpers;
 using System.Text;
@@ -16,30 +19,79 @@ namespace ELearningSystem
             builder.Services.AddControllers();
             builder.Services.AddSignalR();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // مهم: هنجيب Version Provider بعد Build
+                var sp = builder.Services.BuildServiceProvider();
+                var provider = sp.GetRequiredService<IApiVersionDescriptionProvider>();
 
-            // Jwt Config
+                // إنشاء SwaggerDoc لكل Version
+                foreach (var desc in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerDoc(desc.GroupName, new OpenApiInfo
+                    {
+                        Title = "E-Learning API",
+                        Version = desc.GroupName
+                    });
+                }
+
+                // JWT Authentication
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",  // لازم lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                { securityScheme, new string[] {} }
+                });
+            });
+                // Jwt Config
             var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>();
 
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = jwtOptions.Isusser,
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtOptions.Isusser,
 
-                        ValidateAudience = true,
-                        ValidAudience = jwtOptions.Audience,
+                    ValidateAudience = true,
+                    ValidAudience = jwtOptions.Audience,
 
-                        ValidateLifetime = true,
+                    ValidateLifetime = true,
 
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey))
-                    };
-                });
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey))
+                };
+            });
+
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            });
+            builder.Services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV"; // v1 / v2
+                options.SubstituteApiVersionInUrl = true;
+            });
             // إضافة DbContext مع سلسلة الاتصال
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
